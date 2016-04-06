a8a17d87f68dad965627f54f919c11d7ae494081
meeio <meeio.ynn@gmail.com>
2016-04-06 23:40:52 +0800

    1.在rssReader内基本完成从chinanews上下载完整新闻的功能 2.初步定义news类 3.编写新闻网站的properties

---
+++ src/News/News.java
@@ -0,0 +1,18 @@
+package News;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+/**
+ * Created by gaoyuan
+ * Create on 2016/4/6.
+ */
+public class News {
+    private String title;
+    private String describe;
+    private NewsCategories categories;
+    private String content;
+    private String author;
+    private String src;
+    private ArrayList<Path> imagePath;
+}
---
+++ src/News/NewsCategories.java
@@ -0,0 +1,9 @@
+package News;
+
+/**
+ * Created by gaoyuan
+ * Create on 2016/4/6.
+ */
+public enum NewsCategories {
+
+}
